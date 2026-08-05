942e3664f2f2d42610432a400a554d9412b7a84f
ErasmusBill <erasmuschawey12345@gmail.com>
2026-08-05 10:51:14 +0000

    Add PropertyMediaForm and PropertyMediaFormSet to landlords.forms

--- apps/landlords/forms.py
+++ apps/landlords/forms.py
@@ -1,3 +1,27 @@
 from django import forms
+from django.forms import inlineformset_factory
 
-# Create your forms here.
+from apps.home_finder.models import Property, PropertyMedia
+
+
+class PropertyMediaForm(forms.ModelForm):
+    class Meta:
+        model = PropertyMedia
+        fields = [
+            "file",
+            "media_type",
+            "caption",
+            "thumbnail",
+            "order",
+            "is_public",
+        ]
+
+
+PropertyMediaFormSet = inlineformset_factory(
+    Property,
+    PropertyMedia,
+    form=PropertyMediaForm,
+    fields=("file", "media_type", "caption", "thumbnail", "order", "is_public"),
+    extra=1,
+    can_delete=True,
+)
