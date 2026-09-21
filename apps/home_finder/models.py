1f33d6b27705ac8e0ec0ceec9313a2500a414698
Erasmus Chawey Mensah <erasmuschawey12345@gmail.com>
2026-09-21 13:22:59 +0000

    updated property model

--- apps/home_finder/models.py
+++ apps/home_finder/models.py
@@ -74,6 +74,7 @@ class Property(BaseModel):
 
     class PaymentPeriod(models.TextChoices):
         DAILY = "daily", "Daily"
+        HOURLY = "hourly", "Hourly"
         MONTHLY = "monthly", "Monthly"
         YEARLY = "yearly", "Yearly"
 
